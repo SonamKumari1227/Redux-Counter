4bdec8c9fd871c308408bfce28c8b1333172758c
sonam <er.sonam2712kumari@gmail.com>
2024-02-04 19:36:53 +0530

    Complete commit ✅

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement,reset } from './actions/counterAction';
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Redux Counter App</h1>
+      <p>Count: {counter}</p>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <button onClick={() => dispatch(reset())}>Reset</button>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/actions/counterAction.js
@@ -0,0 +1,20 @@
+// src/actions/counterActions.js
+export const increment = () => {
+    return {
+      type: 'INCREMENT',
+    };
+  };
+  
+  export const decrement = () => {
+    return {
+      type: 'DECREMENT',
+    };
+  };
+  
+
+
+  export const reset = () => {
+    return {
+      type: 'RESET'
+    };
+  };
